27812853f5d54386cb0cec2825cc55bd7026381b
Racem Benrhayem <racembenrhayem@insat.u-cartage.tn>
2022-04-15 14:11:42 +0100

    enabling jwt auth for admin

--- src/services/loginHandling.js
+++ src/services/loginHandling.js
@@ -1,6 +1,6 @@
 export const getTokenFromLocalStorage = () => {
     let token = localStorage.getItem("site-fast-admin-token");
-    if(token) {
+    if(token && token.length>0) {
         return JSON.parse(token) ;
     }
     return "" ;
